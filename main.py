e3fd789e41e191a270b7a59e8ad4344e7e1467a4
evoosa <evaosher@gmail.com>
2024-02-29 19:46:23 +0200

    ITS WORKING I AM GOD

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 from playlist_downloader import PlaylistDownloader
 
 if __name__ == '__main__':
--- playlist_downloader.py
+++ playlist_downloader.py
@@ -4,16 +4,22 @@ from consts import PLAYLISTS, BASE_DIR, SPOTIFY_BASE_URL
 import glob
 from datetime import datetime
 from utils import get_logger
+from savify import Savify
+from savify.types import Type, Format, Quality
+from savify.utils import PathHolder
+import dotenv
 
-current_date = datetime.now()
+current_date = datetime.now().strftime('%d_%m_%Y')
+dotenv.load_dotenv()
 
 
 class PlaylistDownloader:
     def __init__(self):
         # create the base output directory
-        self.base_output_dir = os.path.join(BASE_DIR, f"new_music_{current_date.strftime('%d_%m_%Y')}")
+        self.base_output_dir = os.path.join(BASE_DIR, f"new_music_{current_date}")
         self.logger = get_logger(f"{self.base_output_dir}.log")
         self.logger.debug("created logger")
+
         if not os.path.exists(self.base_output_dir):
             os.makedirs(self.base_output_dir)
             self.logger.info(f"created base dir: {self.base_output_dir}")
@@ -32,6 +38,24 @@ class PlaylistDownloader:
         self._create_empty_files(filenames, output_dir)
 
         # download the newly added tracks
+        self._download_playlist(output_dir, f"{SPOTIFY_BASE_URL}{PLAYLISTS[playlist_key]['playlist_id']}")
+        self.logger.info(f"done downloading playlist {playlist_key} to {output_dir}")
+
+        # delete empty files
+        self._delete_empty_files(filenames, output_dir)
+
+    def _download_playlist(self, output_dir, playlist_url):
+        self.logger.info(f"downloading playlist to {output_dir}")
+        savify = Savify(api_credentials=(
+            os.environ.get("SPOTIPY_CLIENT_ID"),
+            os.environ.get("SPOTIPY_CLIENT_SECRET")),
+            quality=Quality.BEST,
+            download_format=Format.MP3,
+            path_holder=PathHolder(downloads_path=output_dir),
+            logger=get_logger(f"{output_dir}_{current_date}.log")
+        )
+        savify.download(playlist_url, query_type=Type.PLAYLIST)
+        self.logger.info(f"done downloading playlist!")
 
     def _get_filenames(self, glob_pattern):
         filenames = []
@@ -39,6 +63,13 @@ class PlaylistDownloader:
             filenames.append(os.path.basename(filepath))
         self.logger.info(f"got {len(filenames)} that can be skipped")
         return filenames
+
+    def _delete_empty_files(self, filenames, output_dir):
+        for filename in filenames:
+            file_to_del = os.path.join(output_dir, filename)
+            if os.path.exists(file_to_del):
+                os.remove(file_to_del)
+        self.logger.info(f"deleted {len(filenames)} empty files")
 
     def _create_empty_files(self, filenames, output_dir):
         for filename in filenames:
