eb53faa9cc438397da64c4b0f4d36e2ba2afe1d8
evoosa <evaosher@gmail.com>
2024-02-29 19:50:57 +0200

    ITS

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import os
+import sys
 
 from playlist_downloader import PlaylistDownloader
 
 if __name__ == '__main__':
+    playlist_key = sys.argv[1]
     pd = PlaylistDownloader()
-    pd.download_playlist_diff("all_time_favs")
+    pd.download_playlist_diff(playlist_key)
