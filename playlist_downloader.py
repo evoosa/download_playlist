71b91261fdc9370dc01c3c8c9280e4eb6760ac9f
evoosa <evaosher@gmail.com>
2024-02-29 14:19:34 +0200

    added

--- playlist_downloader.py
+++ playlist_downloader.py
@@ -3,13 +3,9 @@ import os.path
 from consts import PLAYLISTS, BASE_DIR, SPOTIFY_BASE_URL
 import glob
 from datetime import datetime
+import shutil
 
 current_date = datetime.now()
-
-# Format the date as day_month_year
-formatted_date = current_date.strftime("%d_%m_%Y")
-
-print("Formatted date:", formatted_date)
 
 
 class PlaylistDownloader:
@@ -33,6 +29,13 @@ class PlaylistDownloader:
         for filepath in glob.glob(glob_pattern):
             filenames.append(os.path.basename(filepath))
         return filenames
+
+    @staticmethod
+    def _create_empty_files(filenames, output_dir):
+        for filename in filenames:
+            output_file_path = os.path.join(output_dir, filename)
+            with open(output_file_path, 'w'):
+                pass
 
     def backup_playlists_to_external_drive(self, drive_letter):
         """ backup the downloaded playlists to the given external drive """
