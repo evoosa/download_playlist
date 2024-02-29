3bd9be69d550bc45859afa1dd3035ee2bf3da4f3
evoosa <evaosher@gmail.com>
2024-02-29 15:37:02 +0200

    added playlists

---
+++ playlists.py
@@ -0,0 +1,31 @@
+PLAYLISTS = {
+    "good_old_childhood": {
+        "url": "https://open.spotify.com/playlist/2qCYYjwroRfhIPmSPimh89?si=17cc33b1b7584036",
+        "search_pattern": "*0s/*/"
+    },
+    "80s_gemz": {
+        "url": "https://open.spotify.com/playlist/73RR96vE1uMrv45eanG1ZK?si=b5fd39aa46ee46fa",
+        "search_pattern": "*0s/*/"
+    },
+    "adrenaline_junky": {
+        "url": "https://open.spotify.com/playlist/4Rn9KOaqlzQ0KR054VvHNl?si=0874a374a33a4053",
+        "search_pattern": "my_music/adrenaline_junky/*/"
+    },
+    "hapi_energetic": {
+        "url": "https://open.spotify.com/playlist/4JoD5QW1QkJ8uWEG45F8lk?si=ecd0d0e29634485a",
+        "search_pattern": "my_music/hapi_energetic/*/"
+    },
+    "eyes_sealed_soul_concealed": {
+        "url": "https://open.spotify.com/playlist/1x1h9eDSzKdpk5AA2DLJWb?si=e7c35055f03945e7",
+        "search_pattern": "my_music/eyes_sealed_soul_concealed/*/"
+    },
+    "all_time_favs": {
+        "url": "https://open.spotify.com/playlist/5blvFFETRIf9h7VwbDQ1gg?si=34d1d2e7cfe947a7",
+        "search_pattern": "my_music/all_time_favs/*/"
+    },
+    "french_electronica": {
+        "url": "https://open.spotify.com/playlist/2d7YFJbRsL0Ufy8vcIAKhr?si=f8b4c2a52b604008",
+        "search_pattern": "my_music/French Electronica/*/"
+    }
+
+}
